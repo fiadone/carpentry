ce9435f3f9f2e24c79cd0e2cc1c575e4493fe89b
fiadone <hello@fiad.one>
2022-01-05 11:08:58 +0100

    refactor: router simplified

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
+import barba from '@barba/core'
+import pages from '~/pages'
 import '~/theme'
-import router from '~/router'
 
-window.addEventListener('DOMContentLoaded', router.init)
+window.addEventListener('DOMContentLoaded', () => {
+  let currentPage
+    
+  barba.hooks.beforeEnter(async ({ next }) => {
+    const importPage = pages[next.namespace] || pages.default
+    const { default: Page } = await importPage()
+    currentPage = new Page(next.container)
+  })
+  
+  barba.hooks.afterLeave(() => currentPage.destroy())
+  
+  barba.init({
+    transitions: [{
+      once: () => currentPage.transition.once(),
+      enter: () => currentPage.transition.enter(),
+      leave: () => currentPage.transition.leave()
+    }]
+  })
+})
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
+/**
+ * By default the pages are lazy loaded via dynamic import.
+ * Look at https://vitejs.dev/guide/features.html#glob-import to learn more.
+ */
 const pages = import.meta.glob('./**/index.js')
 
-export default Object.entries(pages).reduce((acc, [filename, importPage]) => {
+export default Object.entries(pages).reduce((acc, [filename, importHandler]) => {
   const key = filename.replace('/index.js', '').split('/').pop()
-  acc[key] = importPage
-  return acc
+  return { ...acc, [key]: importHandler }
 }, {})
